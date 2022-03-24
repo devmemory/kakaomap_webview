c752d3c32a36b01cd95dd4a1d74cfd192804a416
Seongjun Kim <isair.zun@gmail.com>
2022-03-25 03:46:01 +0900

    customStyle example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -47,13 +47,13 @@ class KakaoMapTest extends StatelessWidget {
                   strokeWeight: 2.5,
                   strokeColorOpacity: 0.9),
               // overlayText: '카카오!',
-              customOverlayStyle: '''<style>
-              .customoverlay {position:relative;bottom:85px;border-radius:6px;border: 1px solid #ccc;border-bottom:2px solid #ddd;float:left;}
+              customStyle: '''
+.customoverlay {position:relative;bottom:85px;border-radius:6px;border: 1px solid #ccc;border-bottom:2px solid #ddd;float:left;}
 .customoverlay:nth-of-type(n) {border:0; box-shadow:0px 1px 2px #888;}
 .customoverlay a {display:block;text-decoration:none;color:#000;text-align:center;border-radius:6px;font-size:14px;font-weight:bold;overflow:hidden;background: #d95050;background: #d95050 url(https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/arrow_white.png) no-repeat right 14px center;}
 .customoverlay .title {display:block;text-align:center;background:#fff;margin-right:35px;padding:10px 15px;font-size:14px;font-weight:bold;}
 .customoverlay:after {content:'';position:absolute;margin-left:-12px;left:50%;bottom:-12px;width:22px;height:12px;background:url('https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/vertex_white.png')}
-              </style>''',
+              ''',
               customOverlay: '''
 var content = '<div class="customoverlay">' +
     '  <a href="https://map.kakao.com/link/map/11394059" target="_blank">' +
